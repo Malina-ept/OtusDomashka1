eee486b64e4d12fbf2f98be9346d3bc79b5d0db8
Malina-ept <75109750+Malina-ept@users.noreply.github.com>
2021-05-14 09:52:35 +0300

    fix_lesson7

--- src/test/java/TestLesson7.java
+++ src/test/java/TestLesson7.java
@@ -7,7 +7,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -15,6 +18,12 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.assertEquals;
 
 public class TestLesson7 {
+
+    private static final String EXPECTED_OTUS_CONTACTS_TEXT = "125167, г. Москва, Нарышкинская аллея., д. 5, стр. 2, тел. +7 499 938-92-02";
+    private static final String OTUS_WEBSITE_HOME_PAGE = "https://otus.ru";
+
+    //@FindBy(xpath = "//header//a[@href='/contacts/']")
+    //private WebElement contactsButton;
 
     private Logger logger = LogManager.getLogger(TestLesson7.class);
     protected WebDriver driver;
@@ -35,25 +44,24 @@ public class TestLesson7 {
 
     @Test
     public void checkTitle() {
-        driver.get("https://otus.ru");
+        driver.get(OTUS_WEBSITE_HOME_PAGE);
         logger.info("Сайт открыт");
         logger.info("Проверяем title страницы");
         String actualTitle = driver.getTitle();
         String expectedTitle = "Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям";
-        assertEquals("Текст тайтла соответствует ожидаемому", expectedTitle, actualTitle);
+        assertEquals("Текст тайтла не соответствует ожидаемому", expectedTitle, actualTitle);
     }
 
     @Test
     public void checkTitleContacts() {
-        driver.get("https://otus.ru");
+        driver.get(OTUS_WEBSITE_HOME_PAGE);
         logger.info("Сайт открыт");
-        // driver.get("https://otus.ru/contacts");
         driver.findElement(By.xpath("//header//a[@href='/contacts/']")).click();
         logger.info("Открыта страница Контакты");
         logger.info("Проверяем title страницы Контакты");
         String actualTitle = driver.getTitle();
         String expectedTitle = "Контакты | OTUS";
-        assertEquals("Текст тайтла страницы Контакты соответствует ожидаемому", expectedTitle, actualTitle);
+        assertEquals("Текст тайтла страницы Контакты не соответствует ожидаемому", expectedTitle, actualTitle);
     }
 
 
@@ -61,13 +69,12 @@ public class TestLesson7 {
     public void checkAdressInContacts() {
         driver.get("https://otus.ru/contacts");
         logger.info("Открыта страница Контакты");
-        assertEquals( "125167, г. Москва, Нарышкинская аллея., д. 5, стр. 2, тел. +7 499 938-92-02", driver.findElement(By.cssSelector("#__next > div.Page__Container-sc-2em8v9-0.hYCfxl > div > div.Page__Content-sc-2em8v9-1.iupdqh > div.Layout-sc-1hmcglv-0.hdLBmo > div:nth-child(3) > div.styles__Content-c0qfa0-5.gTwLpc")).getText());
-        logger.info("Адрес соответствует ожидаемому");
+        assertEquals( "Адрес не соответствует ожидаемому",EXPECTED_OTUS_CONTACTS_TEXT, driver.findElement(By.cssSelector("#__next > div.sc-2em8v9-0.etvoKo > div.sc-2em8v9-1.kiwtMy > div.sc-1hmcglv-0.iiFycX > div:nth-child(3) > div.c0qfa0-5.cXQVNI")).getText());
     }
 
     @Test
     public void checkOpenMaxScreen() {
-        driver.get("https://otus.ru");
+        driver.get(OTUS_WEBSITE_HOME_PAGE);
         driver.manage().window().maximize();
         logger.info("Открыто окно браузера на полный экран");
         logger.info(driver.manage().window().getSize());
@@ -75,24 +82,26 @@ public class TestLesson7 {
 
     @Test
     public void checkFAQ() {
-        driver.get("https://otus.ru");
+        driver.get(OTUS_WEBSITE_HOME_PAGE);
         logger.info("Сайт открыт");
         driver.findElement(By.xpath("//header//a[@href='/faq/']")).click();
         logger.info("Открыта страница FAQ");
         driver.findElement(By.cssSelector("body > div.body-wrapper > div > div.container.container-padding-bottom.js-faq > div.container__row.js-tabs > div:nth-child(2) > div:nth-child(4) > div.faq-question__question.js-faq-question-question")).click();
-        assertEquals( "Программу курса в сжатом виде можно увидеть на странице курса после блока с преподавателями. Подробную программу курса можно скачать кликнув на “Скачать подробную программу курса”", driver.findElement(By.cssSelector("body > div.body-wrapper > div > div.container.container-padding-bottom.js-faq > div.container__row.js-tabs > div:nth-child(2) > div:nth-child(4) > div.faq-question__answer.js-faq-answer")).getText());
+        String expectedProgram = "Программу курса в сжатом виде можно увидеть на странице курса после блока с преподавателями. Подробную программу курса можно скачать кликнув на “Скачать подробную программу курса”";
+        assertEquals("Текст не соответствует ожидаемому", expectedProgram, driver.findElement(By.cssSelector("body > div.body-wrapper > div > div.container.container-padding-bottom.js-faq > div.container__row.js-tabs > div:nth-child(2) > div:nth-child(4) > div.faq-question__answer.js-faq-answer")).getText());
         logger.info("Текст соответствует ожидаемому");
 
     }
 
     @Test
-    public void Subscription() throws InterruptedException {
-        driver.get("https://otus.ru");
+    public void checkSubscription() throws InterruptedException {
+        driver.get(OTUS_WEBSITE_HOME_PAGE);
         logger.info("Сайт открыт");
         driver.findElement(By.cssSelector(".input.footer2__subscribe-input")).clear();
         logger.info("Поле email очищено");
         driver.findElement(By.cssSelector(".input.footer2__subscribe-input")).sendKeys("test@test.com");
-        WebDriverWait wait = new WebDriverWait(driver,10);
+        //WebDriverWait wait = new WebDriverWait(driver,10);
+        //wait.until(ExpectedConditions.visibilityOf((WebElement) By.cssSelector(".footer2__subscribe-button.button.button_blue.button_as-input")));
         driver.findElement(By.cssSelector(".footer2__subscribe-button.button.button_blue.button_as-input")).click();
         logger.info("Нажата кнопка Подписаться");
         assertEquals( "Вы успешно подписались", driver.findElement(By.cssSelector("body > div.body-wrapper > div > footer > div > div > div.footer2__content > div > div:nth-child(3) > p.subscribe-modal__success")).getText());
@@ -101,14 +110,16 @@ public class TestLesson7 {
     }
 
     @Test
-    public void Tele2() throws InterruptedException {
+    public void checkTheSearchByNumberTele2() throws InterruptedException {
         driver.get("https://msk.tele2.ru/shop/number");
         logger.info("Сайт Tele2 открыт");
         driver.findElement(By.id("searchNumber")).sendKeys("97");
         logger.info("введены 97 в поиск номера");
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        driver.findElement(By.cssSelector("#root > div > div.height-holder-mobile > div > div > div > div > div.main-content > div:nth-child(1) > div > div > div > div.catalog-numbers.with-overlay.overlay-big > div > div > div.center.left-xs > a")).click();
-        logger.info("Появилась кнопка Показать еще");
+        //WebDriverWait wait = new WebDriverWait(driver, 10);
+        //wait.until(ExpectedConditions.visibilityOf((WebElement) By.cssSelector("#root > div > div.height-holder-mobile > div > div > div > div > div.main-content > div:nth-child(1) > div > div > div > div.catalog-numbers.with-overlay.overlay-big > div > div > div.center.left-xs > a")));
+        assertEquals( "Кнопка 'Показать еще' не появилась", "Показать еще", driver.findElement(By.cssSelector("#root > div > div.height-holder-mobile > div > div > div > div > div.main-content > div:nth-child(1) > div > div > div > div.catalog-numbers.with-overlay.overlay-big > div > div > div.center.left-xs > a")).getText());
+        logger.info("Появилась кнопка 'Показать еще'");
+
     }
 }
 
--- src/test/java/TestLesson8.java
+++ src/test/java/TestLesson8.java
@@ -3,16 +3,18 @@ import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Instant;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
@@ -21,13 +23,18 @@ public class TestLesson8 {
 
     private Logger logger = LogManager.getLogger(TestLesson8.class);
     protected WebDriver driver;
+    private Instant wait;
+
+    //@FindBy(xpath = "//input[@id='1907_325lg']")
+    //private WebElement checkboxLG;
 
     @Before
     public void StartUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         logger.info("Драйвер поднят");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
     }
 
     @After
@@ -37,40 +44,53 @@ public class TestLesson8 {
     }
 
     @Test
-    public void check220() throws InterruptedException {
-        driver.get("https://www.220-volt.ru/");
+    public void compareTwoProductsInCitylink() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver,10);
+
+        driver.get("https://www.citilink.ru/");
         logger.info("Сайт открыт");
         driver.manage().window().maximize();
         logger.info("Открыто окно браузера на полный экран");
-        //driver.findElement(By.cssSelector("body > div.page-container > div.transition-bg-color.bg-black-0.box-relative.box-top.phspace-10.col-12.box-left.panel.box-float-none.z-index-1000 > div > div.panel-catalog.box-relative.box-inline.v-top.col-3.text-left > ul > li > ul > li:nth-child(1)")).click();
-        WebElement popover = driver.findElement(By.cssSelector("body > div.page-container > div.transition-bg-color.bg-black-0.box-relative.box-top.phspace-10.col-12.box-left.panel.box-float-none.z-index-1000 > div > div.panel-catalog.box-relative.box-inline.v-top.col-3.text-left > ul > li > ul > li:nth-child(1)"));
+        logger.info("Переходим в раздел Бытовая техника-Стиральные машины");
+        driver.findElement(By.xpath("//div[@class='MainHeader__catalog']")).click();
+
+        WebElement openHomeAppliances = driver.findElement(By.xpath("//svg[@class='CatalogMenu__category-icon Icon']"));
         Actions action = new Actions(driver);
-        action.moveToElement(popover).pause(400L).perform();
-        //Thread.sleep(1000);
-        WebDriverWait wait = new WebDriverWait(driver,10);
-        driver.findElement(By.cssSelector("body > div.page-container > div.transition-bg-color.bg-black-0.box-relative.box-top.phspace-10.col-12.box-left.panel.box-float-none.z-index-1000 > div > div.panel-catalog.box-relative.box-inline.v-top.col-3.text-left > ul > li > ul > li:nth-child(1) > ul > li > div > ol > li:nth-child(23)")).click();
-        logger.info("Открыт раздел Перфораторы");
-        driver.findElement(By.cssSelector("#producer_16")).click();
-        driver.findElement(By.cssSelector("#producer_473")).click();
-        logger.info("Выбраы марки Makita и Зубр");
-        driver.findElement(By.cssSelector("#filterSubm")).click();
-        logger.info("Применен фильтра с  марками Makita и Зубр");
-        File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        saveFile(file);
-        driver.findElement(By.cssSelector("body > div.box-relative.phbspace-10.box-960.box-center.container.content.content-main.listing > div.bg-white.radius-5.box.pspace-20 > div.mhtspace-20 > div.columns.columns-page.mhtspace-20 > div.column.column-page.right > div.filter-content.mhbspace-10 > ul > li.filter-content-sort.col-20p > noindex > span > span > span.selection > span")).click();
-        Thread.sleep(10000);
-        driver.findElement(By.cssSelector(".select2-results__option.select2-results__option--highlighted")).click();
-        Thread.sleep(10000);
-        logger.info("Сортировка по Возрастанию цены");
-        driver.findElement(By.cssSelector("#product-list > li:nth-child(1) > div > div.new-item-list-compare.custom-ui-compare.compare")).click();
-        driver.findElement(By.cssSelector("#modal-content-placeholder > div > table > tbody > tr:nth-child(3) > td.buttons > div > a")).click();
-        logger.info("Добавлен к сравнению первый Зубр");
-        driver.findElement(By.cssSelector("#product-list > li:nth-child(9) > div > div.new-item-list-compare.custom-ui-compare.compare")).click();
-        logger.info("Добавлен к сравнению первый Makita");
-        Thread.sleep(10000);
-        driver.findElement(By.cssSelector("#modal-content-placeholder > div > table > tbody > tr:nth-child(5) > td:nth-child(2) > div > a")).click();
-        logger.info("Переход к сравнению");
-        Thread.sleep(10000);
+        action.moveToElement(openHomeAppliances).pause(800L).perform();
+
+        driver.findElement(By.xpath("//div[@class='CatalogMenu__subcategory-item'][3]")).click();
+        logger.info("Открыта страница Стиральные машины");
+
+        logger.info("Выберем на странице чекбокс BOSCH");
+        driver.findElement(By.xpath("//input[@id='1907_325bosch']")).click();
+        //Thread.sleep(10000);
+        //By checkboxLG = new WebElement(By.xpath("//input[@id='1907_325lg']"));
+        logger.info("Чекбокс BOSCH выбран");
+
+        //WebElement checkboxLG = (WebElement) By.xpath("//input[@id='1907_325lg']");
+        logger.info("Выберем на странице чекбокс Electrolux");
+        //wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='1907_325electrolux']")));
+        driver.findElement(By.xpath("//input[@id='1907_325electrolux']")).click();
+        logger.info("Чекбокс Electrolux выбран");
+        //Thread.sleep(10000);
+
+        //WebElement sortPrice = (WebElement) By.xpath("//div[@data-alias='price']");
+        logger.info("Отсортируем полученные результаты по цене");
+        //wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@data-alias='price']")));
+        driver.findElement(By.xpath("//div[@data-alias='price']")).click();
+
+
+        logger.info("Выберем первую в списке BOSCH");
+        driver.findElement(By.xpath("(//*[@class='ProductGroupList js--ProductGroupList']//div[contains(@data-params, 'BOSCH')]//*[contains(@class, 'ProductCardButton__button-add')])[1]")).click();
+        logger.info("А теперь Выберем первую в списке Electrolux");
+        driver.findElement(By.xpath("(//*[@class='ProductGroupList js--ProductGroupList']//div[contains(@data-params, 'ELECTROLUX')]//*[contains(@class, 'ProductCardButton__button-add')])[1]")).click();
+        logger.info("Выбраны для сравнения первая BOSCH и первая Electrolux");
+
+        logger.info("Перейдём на страницу сравнения");
+        driver.findElement(By.xpath("//div[@class='HeaderMenu__buttons  HeaderMenu__buttons_compare']")).click();
+        logger.info("Открыта страница сравнения");
+
+        Thread.sleep(1000);
 
     }
 
